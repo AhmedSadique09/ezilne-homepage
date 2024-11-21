5ea18dd62cbe110fecddcf5c7bce636ec97bb3ec
Ghulam Ahmed <dev.ahmed09@gmail.com>
2024-11-21 23:18:48 +0500

    .

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const hamburger = document.querySelector('.hamburger');
-    const navLinks = document.querySelector('.nav-links');
-    
-    hamburger.addEventListener('click', () => {
-      navLinks.classList.toggle('show');
-    });    
-  });
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('navLinks');
+const searchIcon = document.getElementById('searchIcon');
+const searchBarContainer = document.getElementById('searchBarContainer');
 
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+  hamburger.classList.toggle('active');
+});
+
+// If on mobile, show the search bar below the "16th Anniversary" menu item when search icon is clicked
+searchIcon.addEventListener('click', () => {
+  if (window.innerWidth <= 768) {
+    searchBarContainer.style.display = 'block';
+  }
+});
